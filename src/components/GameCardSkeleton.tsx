9ed1d94ac310a18989c4a58fa8ad6a64684dceb9
jamshale <jamiehalebc@gmail.com>
2023-03-20 10:47:33 -0700

    Creating a generic data fetching hook

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const GameCardSkeleton = () => {
   return (
     <Card>
-      <Skeleton height="200px" />
+      <Skeleton height="200px" width="500px" />
       <CardBody>
         <SkeletonText />
       </CardBody>
