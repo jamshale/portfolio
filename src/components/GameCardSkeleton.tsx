e1d5ebb82233cd27b3f95eea36146f22b5dd61ad
jamshale <jamiehalebc@gmail.com>
2023-03-20 10:59:40 -0700

    Display genres

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const GameCardSkeleton = () => {
   return (
     <Card>
-      <Skeleton height="200px" width="500px" />
+      <Skeleton height="200px" />
       <CardBody>
         <SkeletonText />
       </CardBody>
